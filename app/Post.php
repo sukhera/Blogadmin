37b6f9b2b4ec39a72ded887dd65f661bdab3d358
Ahmed Sukhera <ahmed.sukhera@gmail.com>
2019-02-04 17:14:20 -0500

    Removed the use of session

--- app/Post.php
+++ app/Post.php
@@ -8,64 +8,29 @@ class Post extends Model {
 
     protected $fillable = ['title', 'content'];
 
-    public function likes(){
-
-        return $this->hasMany('App\Like');
+    public function likes()
+    {
+        return $this->hasMany('App\Like', 'post_id');
     }
 
-    public function tags(){
-
-        return $this->belongsToMany('App\Tag')->withTimestamps();
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag', 'post_tag', 'post_id', 'tag_id')->withTimestamps();
     }
 
     public function user()
     {
-        return $this->belongsTo('App\User', 'user_id');
+        return $this->belongsTo('App\User');
     }
 
-    public function getPosts($session){
-        if(!$session->has('posts')){
-        $posts = $this->createDummyPosts();
-        $session->put('posts', $posts);
-
-        }
-        return $session->get('posts');
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = strtolower($value);
     }
 
-    private function createDummyPosts(){
-     $posts = [
-        ['title' => 'Learning Laravel',
-            'content' => 'This is the first step'],
-        ['title' => 'second Laravel',
-            'content' => 'This is the second step']
-        ];
-        return $posts;
-    }
-
-    public function getPost($session,$id){
-        if(!$session->has('posts')){
-            $posts = $this->createDummyPosts();
-            $session->put('posts', $posts);
-        }
-        return $session->get('posts')[$id];
-    }
-
-    public function addPost($session, $title, $content){
-        if(!$session->has('posts')){
-            $posts = $this->createDummyPosts();
-            $session->put('posts', $posts);
-        }
-        $posts = $session->get('posts');
-        array_push($posts, ['title' => $title, 'content' => $content ]);
-        $session->put('posts',$posts);
-
-    }
-
-    public function editPost($session, $id, $title, $content){
-        $posts = $session->get('posts');
-        $posts[$id] = ['title' => $title, 'content' => $content];
-        $session->put('posts', $posts);
-
+    public function getTitleAttribute($value)
+    {
+        return strtoupper($value);
     }
 
 }
